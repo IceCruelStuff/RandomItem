6a43c2a11b595bc83d24e78c28415060b3f63039
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2019-12-01 11:49:42 -0800

    Create Gift.php

---
+++ src/RandomItem/Gift.php
@@ -0,0 +1,18 @@
+<?php
+namespace RandomItem;
+use use pocketmine\event\Listener;
+use pocketmine\Server;
+class Gift implements Listener {
+  public function __construct(Main $plugin) {
+    $this->plugin = $plugin;
+    $this->start = false;
+    parent::__construct(Main $plugin);
+  }
+  public function onRun(int $currentTick) : void{
+    if($this->start) {
+      $this->plugin->giveAll();
+    } else {
+      $this->start = true;
+    }
+  }
+}
