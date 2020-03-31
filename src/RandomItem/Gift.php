163995b44c3e0a5c5fb2353f6c2caf2161628abb
IceCruelStuff <minecraftcool991@gmail.com>
2020-03-30 18:42:30 -0700

    fix plugin

--- src/RandomItem/Gift.php
+++ src/RandomItem/Gift.php
@@ -1,20 +1,23 @@
 <?php
+
 namespace RandomItem;
 
 use pocketmine\event\Listener;
 use pocketmine\Server;
 
 class Gift implements Listener {
-  public function __construct(Main $plugin) {
-    $this->plugin = $plugin;
-    $this->start = false;
-    parent::__construct(Main $plugin);
-  }
-  public function onRun(int $currentTick) : void{
-    if($this->start) {
-      $this->plugin->giveAll();
-    } else {
-      $this->start = true;
-    }
-  }
+	
+	public function __construct(Main $plugin) {
+		$this->plugin = $plugin;
+		$this->start = false;
+		parent::__construct(Main $plugin);
+	}
+	
+	public function onRun(int $currentTick) : void{
+		if($this->start) {
+			$this->plugin->giveAll();
+		} else {
+			$this->start = true;
+		}
+	}
 }
--- src/RandomItem/Main.php
+++ src/RandomItem/Main.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace RandomItem;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
@@ -7,57 +9,65 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use pocketmine\item\Item;
+
 class Main extends PluginBase {
-  public function onEnable() : void{
-    $this->saveDefaultConfig();
-    $c = $this->getConfig()->getAll();
-    $t = $c["Interval"] * 1200;
-    $num = 0;
-    foreach ($c["Items"] as $i) {
-      $r = explode(":",$i);
-      $this->itemdata[$num] = array("id" => $r[0],"meta" => $r[1],"amount" => $r[2]);
-      $num++;
-    }
-    $this->getServer()->getScheduler()->scheduleRepeatingTask(new Gift($this),$t);
-  }
-  public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-    if((strtolower($command->getName()) == "gift") && isset($args[0])) {
-      if($this->getServer()->getPlayer($args[0]) instanceof Player) {
-        $d = $this->generateData();
-        $p = $this->getServer()->getPlayer($args[0]);
-        $this->give($p,$d);
-        $p->sendMessage("Random item given! (" . $data["id"] . ":" . $data["meta"] . ")");
-      } else {
-        $issuer->sendMessage("Player not connected.");
-      }
-      return true;
-    } else if((strtolower($cmd->getName()) == "gift") && !(isset($args[0]))) {
-      $this->giveAll();
-      return true;
-    } else {
-      return false;
-    }
-  }
-  public function give($p,$data) {
-    if($p instanceof Player && ($p->hasPermission("randomitem") || $p->hasPermission("randomitem.receive"))) {
-      $item = new Item($data["id"],$data["meta"],$data["amount"]);
-      $p->getInventory()->addItem($item);
-    }
-  }
-  public function giveAll() {
-    $data = $this->generateData();
-    $this->broadcast("Random item given! (" . $data["id"] . ":" . $data["meta"] . ")");
-    foreach($this->getServer()->getOnlinePlayers() as $p) {
-      $this->give($p,$data);
-    }
-  }
-  public function broadcast($m) {
-    foreach($this->getServer()->getOnlinePlayers() as $p) {
-      $p->sendMessage($m);
-    }
-    $this->getLogger()->info(TextFormat::YELLOW . $m);
-  }
-  public function generateData() {
-    return $this->itemdata[mt_rand(0,(count($this->itemdata) - 1))];
-  }
+	
+	public function onEnable() : void{
+		$this->saveDefaultConfig();
+		$c = $this->getConfig()->getAll();
+		$t = $c["Interval"] * 1200;
+		$num = 0;
+		foreach ($c["Items"] as $i) {
+			$r = explode(":",$i);
+			$this->itemdata[$num] = array("id" => $r[0],"meta" => $r[1],"amount" => $r[2]);
+			$num++;
+		}
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Gift($this),$t);
+	}
+	
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if((strtolower($command->getName()) == "gift") && isset($args[0])) {
+			if($this->getServer()->getPlayer($args[0]) instanceof Player) {
+				$d = $this->generateData();
+				$p = $this->getServer()->getPlayer($args[0]);
+				$this->give($p,$d);
+				$p->sendMessage("Random item given! (" . $data["id"] . ":" . $data["meta"] . ")");
+			} else {
+				$issuer->sendMessage("Player not connected.");
+			}
+			return true;
+		} else if((strtolower($cmd->getName()) == "gift") && !(isset($args[0]))) {
+			$this->giveAll();
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public function give($p,$data) {
+		if($p instanceof Player && ($p->hasPermission("randomitem") || $p->hasPermission("randomitem.receive"))) {
+			$item = new Item($data["id"],$data["meta"],$data["amount"]);
+			$p->getInventory()->addItem($item);
+		}
+	
+	}
+	
+	public function giveAll() {
+		$data = $this->generateData();
+		$this->broadcast("Random item given! (" . $data["id"] . ":" . $data["meta"] . ")");
+		foreach($this->getServer()->getOnlinePlayers() as $p) {
+			$this->give($p,$data);
+		}
+	}
+	
+	public function broadcast($m) {
+		foreach($this->getServer()->getOnlinePlayers() as $p) {
+			$p->sendMessage($m);
+		}
+		$this->getLogger()->info(TextFormat::YELLOW . $m);
+	}
+	
+	public function generateData() {
+		return $this->itemdata[mt_rand(0,(count($this->itemdata) - 1))];
+	}
 }
